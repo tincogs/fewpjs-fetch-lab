5a2cf50b74d9884a29fb4f361bc86482894e982b
tincogs <tomoday91@gmail.com>
2020-10-28 13:18:01 -0500

    commit tree 6ad222da7fed076df564b51906b437aed5a9f23f

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books').then(function(response){
+    return response.json();
+  }).then(function(json){
+    return renderBooks(json)
+  })
 }
 
 function renderBooks(books) {
